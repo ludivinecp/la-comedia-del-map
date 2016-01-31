bebc90fc93ec197a15ad198b6f126c0ef6d096ca
ludivinecp <coipel.ludivine@gmail.com>
2016-02-01 00:10:28 +0100

    group price: 10% less from 10 people

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,6 +6,8 @@ class Ticket < ActiveRecord::Base
   validates_presence_of :date
   validates_presence_of :hour
 
+ 
+
   def remaining_places
     play = Play.find(self.plays_id)
     count = play.theater.number_of_places
@@ -19,10 +21,22 @@ class Ticket < ActiveRecord::Base
 
   def final_price
     play = Play.find(self.plays_id)
-    (play.base_price * self.number_of_people) /100
+    if self.number_of_people >= 10
+      self.group_price
+    else
+    (play.base_price  * self.number_of_people) / 100
+    end
+  end
+
+  def group_price
+    play = Play.find(self.plays_id)
+    total = play.base_price * self.number_of_people / 100
+    total - (total * 0.10) 
+   
   end
 
   def mailer_booking_confirmation
     BookingMailer.new_booking(self).deliver_now
   end 
+
 end
